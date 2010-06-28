8383334bc50a511df36cdce0ab4704ba476dc363
OHASHI Hideya <ohachige@gmail.com>
2010-06-28 18:54:37 +0900

    implement some dummy requests

--- lib/twtester/web.rb
+++ lib/twtester/web.rb
@@ -1,9 +1,33 @@
 require 'sinatra/base'
+require 'json'
 
 module TwTester
   class Web < Sinatra::Base
-    get '/' do
-      "Hello world!"
+    get '/1/statuses/home_timeline.json' do
+      timeline = [
+        {
+          'text' => 'hello',
+          'in_reply_to_status_id' => 1234,
+          'user' => {
+            'screen_name' => 'someone',
+            'verified' => true,
+            'protected' => true,
+          },
+        }
+      ]
+      timeline.to_json
+    end
+
+    post '/1/statuses/update.json' do
+      status = params['status']
+      p status
+      response = [
+      ]
+      response.to_json
+    end
+
+    get '/1/account/rate_limit_status.json' do
+      {'remaining_hits' => 150}.to_json
     end
   end
 end
